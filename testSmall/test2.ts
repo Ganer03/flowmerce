67e5fb84b99717d9026702b7008f1cd23f6b70b7
Ganer03 <ivimiamia@gmail.com>
2026-03-24 12:51:09 +0300

    обнова теста

--- testSmall/test2.ts
+++ testSmall/test2.ts
@@ -1,25 +1,134 @@
 import fs from "fs";
 import { HandlebarsStreamWriter } from "@flowmerce/core";
 
-function logMemory(label: string) {
-    const used = process.memoryUsage()
-    console.log(label, {
-        rss: Math.round(used.rss / 1024 / 1024) + " MB",
-        heapUsed: Math.round(used.heapUsed / 1024 / 1024) + " MB",
-    })
+function generateBigString(i: number) {
+  return (i + "_").repeat(5 * 1024) // ~10KB уникальная строка
 }
+
+function formatMB(bytes: number) {
+  return Math.round(bytes / 1024 / 1024)
+}
+
+function createMemoryTracker() {
+  const start = process.memoryUsage()
+
+  let peak = { ...start }
+
+  function track() {
+    const current = process.memoryUsage()
+
+    for (const key of Object.keys(current) as (keyof typeof current)[]) {
+      if (current[key] > peak[key]) {
+        peak[key] = current[key]
+      }
+    }
+  }
+
+  function printFinal(label: string) {
+    const end = process.memoryUsage()
+
+    console.log(`\n=== ${label} MEMORY SUMMARY ===`)
+
+    console.table({
+      start: {
+        rss: formatMB(start.rss),
+        heapUsed: formatMB(start.heapUsed),
+        heapTotal: formatMB(start.heapTotal),
+        external: formatMB(start.external),
+      },
+      peak: {
+        rss: formatMB(peak.rss),
+        heapUsed: formatMB(peak.heapUsed),
+        heapTotal: formatMB(peak.heapTotal),
+        external: formatMB(peak.external),
+      },
+      end: {
+        rss: formatMB(end.rss),
+        heapUsed: formatMB(end.heapUsed),
+        heapTotal: formatMB(end.heapTotal),
+        external: formatMB(end.external),
+      },
+      diff: {
+        rss: formatMB(end.rss - start.rss),
+        heapUsed: formatMB(end.heapUsed - start.heapUsed),
+        heapTotal: formatMB(end.heapTotal - start.heapTotal),
+        external: formatMB(end.external - start.external),
+      },
+    })
+  }
+
+  return { track, printFinal }
+}
+
 
 function generateProducts(count: number) {
     const arr = []
     for (let i = 0; i < count; i++) {
         arr.push({
-            productId: i,
-            variantId: i,
-            title: "Product " + i,
-            vendorId: 1,
-            categoryId: 1,
+            "productId": 718165 + i,
+            "variantId": 718165 + i,
+            "url": "https://www.thepoizon.ru/product/8900049401587678" + i,
+            "title": generateBigString(i),
+            "description": i,
+            "vendorCode": "3WD30301349" + i,
+            "categoryId": 6573 + i,
+            "vendorId": 168 + i,
+            "images": [
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/397cdde90d2f4e7bab1ef156e4b1e69e.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/2025061613/60288da6932de420bf4b84144cde2fcb.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/eaff15743b9a465895400713a4e0d04b.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/5cf8a86617254c8aa02bbad789b9cff5.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/2025061613/1cdfb2e54643ca68ee80a315fc58ea22.jpg" + i
+            ],
+            "favoriteCount": i,
+            "countryOfOrigin": "Китай" + i,
+            "properties": [
+                {
+                    "key": "Верхняя высота",
+                    "value": "Низкий вырез"
+                },
+                {
+                    "key": "Стиль носка",
+                    "value": "Круглая голова"
+                },
+                {
+                    "key": "Применимый сезон",
+                    "value": "Весна, лето, осень, зима"
+                },
+                {
+                    "key": "Тип каблука",
+                    "value": "Толстое дно"
+                },
+                {
+                    "key": "Основной цвет",
+                    "value": "Розовый"
+                },
+                {
+                    "key": "Дата выхода",
+                    "value": "2023.10"
+                },
+                {
+                    "key": "Подбор цветов",
+                    "value": "Розовый"
+                },
+                {
+                    "key": "Цена предложения",
+                    "value": "¥1628"
+                },
+                {
+                    "key": "Основной номер товара",
+                    "value": "3WD30301349"
+                }
+            ],
+            "seriesName": "" + i,
+            "relatedProducts": [],
+            "gender": "Женщины" + i,
+            "sizes": [],
+            "vat": "NO_VAT" + i,
+            "currency": "RUB" + i,
+            "keywords": [],
+            "vendor": "On" + i,
             price: i,
-            currency: "USD",
         })
     }
     return arr
@@ -27,10 +136,12 @@ function generateProducts(count: number) {
 
 async function testWithArray() {
     console.log("\n=== ARRAY TEST ===")
+    
+    const tracker = createMemoryTracker()
 
-    const products = generateProducts(100_000)
+    const products = generateProducts(1_000_000)
 
-    logMemory("before")
+    // logMemory("before")
 
     const start = Date.now()
 
@@ -51,7 +162,7 @@ async function testWithArray() {
     let i = 0
     for (const p of result) {
         if (i % 10000 === 0) {
-            logMemory(`array step ${i}`)
+            tracker.track()
         }
 
         await hsw.putData(p)
@@ -62,7 +173,7 @@ async function testWithArray() {
 
     const end = Date.now()
 
-    logMemory("after")
+  tracker.printFinal("ARRAY")
 
     console.log("time:", end - start, "ms")
 }
@@ -73,16 +184,73 @@ function delay(ms: number) {
 
 async function* generateProductsStream(count: number) {
     for (let i = 0; i < count; i++) {
-        await delay(1) // убрать для временной проверки
+        // await delay(1) // убрать для временной проверки
 
         yield {
-            productId: i,
-            variantId: i,
-            title: "Product " + i,
-            vendorId: 1,
-            categoryId: 1,
+            "productId": 718165 + i,
+            "variantId": 718165 + i,
+            "url": "https://www.thepoizon.ru/product/8900049401587678" + i,
+            "title": generateBigString(i),
+            "description": i,
+            "vendorCode": "3WD30301349" + i,
+            "categoryId": 6573 + i,
+            "vendorId": 168 + i,
+            "images": [
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/397cdde90d2f4e7bab1ef156e4b1e69e.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/2025061613/60288da6932de420bf4b84144cde2fcb.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/eaff15743b9a465895400713a4e0d04b.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/20250320/5cf8a86617254c8aa02bbad789b9cff5.jpg" + i,
+                "https://cdn.poizon.com/pro-img/origin-img/2025061613/1cdfb2e54643ca68ee80a315fc58ea22.jpg" + i
+            ],
+            "favoriteCount": i,
+            "countryOfOrigin": "Китай" + i,
+            "properties": [
+                {
+                    "key": "Верхняя высота",
+                    "value": "Низкий вырез"
+                },
+                {
+                    "key": "Стиль носка",
+                    "value": "Круглая голова"
+                },
+                {
+                    "key": "Применимый сезон",
+                    "value": "Весна, лето, осень, зима"
+                },
+                {
+                    "key": "Тип каблука",
+                    "value": "Толстое дно"
+                },
+                {
+                    "key": "Основной цвет",
+                    "value": "Розовый"
+                },
+                {
+                    "key": "Дата выхода",
+                    "value": "2023.10"
+                },
+                {
+                    "key": "Подбор цветов",
+                    "value": "Розовый"
+                },
+                {
+                    "key": "Цена предложения",
+                    "value": "¥1628"
+                },
+                {
+                    "key": "Основной номер товара",
+                    "value": "3WD30301349"
+                }
+            ],
+            "seriesName": "" + i,
+            "relatedProducts": [],
+            "gender": "Женщины" + i,
+            "sizes": [],
+            "vat": "NO_VAT" + i,
+            "currency": "RUB" + i,
+            "keywords": [],
+            "vendor": "On" + i,
             price: i,
-            currency: "USD",
         };
     }
 }
@@ -90,7 +258,7 @@ async function* generateProductsStream(count: number) {
 async function testWithStream() {
     console.log("\n=== STREAM TEST ===")
 
-    logMemory("before")
+    const tracker = createMemoryTracker()
 
     const start = Date.now()
 
@@ -103,9 +271,9 @@ async function testWithStream() {
     stream.pipe(fs.createWriteStream("stream.txt"))
 
     let i = 0
-    for await (const p of generateProductsStream(100_000)) {
+    for await (const p of generateProductsStream(6_000_000)) {
         if (i % 10000 === 0) {
-            logMemory(`stream step ${i}`)
+            tracker.track()
         }
 
         await hsw.putData(p)
@@ -116,13 +284,13 @@ async function testWithStream() {
 
     const end = Date.now()
 
-    logMemory("after")
+    tracker.printFinal("STREAM")
 
     console.log("time:", end - start, "ms")
 }
 
 async function run() {
-    await testWithArray()
+    // await testWithArray()
     await testWithStream()
 }
 
